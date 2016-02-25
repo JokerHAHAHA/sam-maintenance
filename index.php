976f3e25a5c814a565c6e2540deffb1e63542682
JokerHAHAHA <liondejude@hotmail.com>
2016-02-25 08:48:05 +0100

    debut du serieux

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+        <link rel="stylesheet" href="bootstrap-3.3.6-dist/css/bootstrap.min.css" />
+
+        <link rel="stylesheet" href="style.css" />
+        <title>SAM | Maintenance Industrielle | Rhone Alpes | Albertville</title>
+    </head>
+
+
+    <body>
+        <p id="num">04 79 37 16 29</p>
+        <header>
+            <img class="navbar-fixed-top" id="imgonglet" src="img/onglet.png" alt="onglet contact de la société sam" />
+            <!--<nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container">
+                    <ul class="nav nav-pills">
+                        <li role="presentation"><a href="#">ACCUEIL</a></li>
+                        <li role="presentation"><a href="#">CHANTIER</a></li>
+                        <li role="presentation"><a href="#">DEVIS</a></li>
+                        <li role="presentation"><a href="#">PARTENAIRES</a></li>
+                        <li role="presentation"><a href="#">BONNES AFFAIRES</a></li>
+                        <li role="presentation"><a href="#">CONTACT</a></li>
+                    </ul>
+                </div>
+            </nav>-->
+
+            <div id="conteneur">
+                <div id="pendulaire">
+                    <img src="img/pendulaire.png" alt="" />               
+                </div>
+                <div id="dechets">
+                    <img src="img/dechets.png" alt="" />
+                </div> 
+                <div id="concasseur">
+                    <img src="img/concasseur.png" alt="" />
+                </div> 
+                <div id="bois">
+                    <img src="img/bois.png" alt="" />
+                </div>
+            </div>
+            <div id="slogan">
+                <p>Slogan de la SAM a mettre ici</p>
+            </div>
+        </header>
+
+
+        <section>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+        </section>
+
+
+        <footer>
+
+        </footer>        
+
+    </body>
