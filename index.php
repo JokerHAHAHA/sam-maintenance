11dfcb3083a1d3195b06243aa3cf1980e570475e
JokerHAHAHA <liondejude@hotmail.com>
2016-02-27 16:16:34 +0100

    header finished

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="bootstrap-3.3.6-dist/css/bootstrap.min.css" />
 
         <link rel="stylesheet" href="style.css" />
+        <link rel="icon" type="image/png" href="img/logo.png" />
         <title>SAM | Maintenance Industrielle | Rhone Alpes | Albertville</title>
     </head>
 
@@ -22,33 +23,45 @@
         <header>
             <div class="onglet navbar-fixed-top">
                 <img id="imgonglet" src="img/onglet.png" alt="onglet contact de la société sam" />
+                <a href="mailto: contact@sam-maintenance.com"><p id="adressemail">contact@sam-maintenance.com</p></a>
                 <p id="num">04 79 37 16 29</p>
             </div>
             <nav class="navbar navbar-default navbar-fixed-top">
-<div class="container">
-<ul class="nav nav-pills">
-<li role="presentation"><a href="#">ACCUEIL</a></li>
-<li role="presentation"><a href="#">CHANTIER</a></li>
-<li role="presentation"><a href="#">DEVIS</a></li>
-<li role="presentation"><a href="#">PARTENAIRES</a></li>
-<li role="presentation"><a href="#">BONNES AFFAIRES</a></li>
-<li role="presentation"><a href="#">CONTACT</a></li>
-</ul>
-</div>
-</nav>
+                <div class="container">
+                    <ul class="nav nav-pills">
+                        <img src="img/logo.png" alt="logo de la societe sam en rhone alpes" />
+                        <li role="presentation"><a href="#">ACCUEIL</a></li>
+                        <li role="presentation"><a href="#">CHANTIERS</a></li>
+                        <li role="presentation"><a href="#">DEVIS</a></li>
+                        <li role="presentation"><a href="#">PARTENAIRES</a></li>
+                        <li role="presentation"><a href="#">BONNES AFFAIRES</a></li>
+                        <li role="presentation"><a href="#">CONTACT</a></li>
+                    </ul>
+                </div>
+            </nav>
 
             <div id="conteneur">
+
                 <div id="pendulaire">
-                    <img src="img/pendulaire.png" alt="" />               
+                    <div id="grispendulaire"></div> 
+                    <h2>BROYEURS PENDULAIRES</h2>
+                    <p  id="txtpendulaire">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus nibh quis urna commodo laoreet. Mauris sit amet hendrerit elit. Pellentesque ut ligula ornare, bibendum metus a, rutrum leo. Quisque mi ante, suscipit a egestas sit amet, condimentum vitae tellus.</p>        
                 </div>
+
                 <div id="dechets">
-                    <img src="img/dechets.png" alt="" />
+                    <div id="grisdechets"></div> 
+                    <h2>BROYEURS DECHETS</h2>
+                    <p  id="txtdechets">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus nibh quis urna commodo laoreet. Mauris sit amet hendrerit elit. Pellentesque ut ligula ornare, bibendum metus a, rutrum leo. Quisque mi ante, suscipit a egestas sit amet, condimentum vitae tellus.</p>   
                 </div> 
                 <div id="concasseur">
-                    <img src="img/concasseur.png" alt="" />
+                    <div id="grisconcasseur"></div> 
+                    <h2>CONCASSEURS</h2>
+                    <p  id="txtconcasseur">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus nibh quis urna commodo laoreet. Mauris sit amet hendrerit elit. Pellentesque ut ligula ornare, bibendum metus a, rutrum leo. Quisque mi ante, suscipit a egestas sit amet, condimentum vitae tellus.</p>   
                 </div> 
                 <div id="bois">
-                    <img src="img/bois.png" alt="" />
+                    <div id="grisbois"></div> 
+                    <h2>BROYEURS BOIS</h2>
+                    <p  id="txtbois">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus nibh quis urna commodo laoreet. Mauris sit amet hendrerit elit. Pellentesque ut ligula ornare, bibendum metus a, rutrum leo. Quisque mi ante, suscipit a egestas sit amet, condimentum vitae tellus.</p>   
                 </div>
             </div>
             <div id="slogan">
@@ -58,14 +71,19 @@
 
 
         <section>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
+
+
+            delai respectés
+            qualité des prduits fournisseurs
+            qualité du travail des equipes SAM
+            40 ans d'expérience
+            Conseils personnalisés
+
+
+
+
+
+
             <p>...</p>
             <p>...</p>
             <p>...</p>
@@ -107,7 +125,9 @@
 
 
         <footer>
-
+            <div id="carte">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5577.396408374497!2d6.372640350015271!3d45.65688210217829!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x64e09b995a937fb5!2sS.a.m+Sarl!5e0!3m2!1sfr!2sfr!4v1456515558257" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
         </footer>        
 
     </body>
