1e0b2393b1bfa64ef5e997fa84adccd8db822d7b
JokerHAHAHA <liondejude@hotmail.com>
2016-02-27 18:52:17 +0100

    Add the section _VALEUR_

--- index.php
+++ index.php
@@ -2,18 +2,14 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
         <link rel="stylesheet" href="bootstrap-3.3.6-dist/css/bootstrap.min.css" />
 
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+
         <link rel="stylesheet" href="style.css" />
+
         <link rel="icon" type="image/png" href="img/logo.png" />
         <title>SAM | Maintenance Industrielle | Rhone Alpes | Albertville</title>
     </head>
@@ -69,60 +65,25 @@
             </div>
         </header>
 
-
-        <section>
-
-
-            delai respectés
-            qualité des prduits fournisseurs
-            qualité du travail des equipes SAM
-            40 ans d'expérience
-            Conseils personnalisés
-
-
-
-
-
-
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
+        <section id="valeurs">
+            <p>LES VALEURS DE LA SAM</p>
+            <div class="z-depth-4" id="delai">
+                <img src="img/delai.png" alt="icone de respect des delais" />
+                <p><span>DELAI</span>Nous sommes conscients des enjeux liés à l'arret d'une machine pour votre entreprise. C'est pourquoi nous nous engageons à respecter nos délais. Nous sommes à votre service afin de valoriser au maximum votre activité.</p>
+            </div>
+            <div class="z-depth-4" id="experience">
+                <img src="img/experience.jpg" alt="icone de grande expérience" />
+                <p><span>10 ANS D'EXPERIENCE</span>La SAM est présente dans le domaine à Albertville depuis plus de 10 ans. Passionnés par le secteur métallurgique, vous pouvez compter sur la SAM pour vos travaux de <em>maintenance industrielle</em>.</p>
+            </div>
+            <div class="z-depth-4" id="qualite">
+                <img src="img/qualite.png" alt="icone de respect de la qualité" />
+                <p><span>QUALITE</span>Nous travaillons avec un objectif de qualité. Fort de notres expérience dans le domaine de la maintenance industrielle, nous choisissons des fournisseurs de qualité. Nos équipes sont formés régulièrement afin d'assurer les meilleurs practiques.</p>
+            </div>
+            <div class="z-depth-4" id="conseils">
+                <img src="img/conseils.png" alt="icone de coneils professionnels" />
+                <p><span>CONSEILS</span>L'enjeu est fort : un broyeur industriel en panne crée rapidement des manques à gagner importants et les investissements sont non négligeables.Il est crucial de faire les bon choix de maintenance, de pièces détachées et de gestion des pannes. Nous vous conseillerons dans vos prises de décision liés à vos problématiques.</p>
+            </div>
         </section>
-
 
         <footer>
             <div id="carte">
